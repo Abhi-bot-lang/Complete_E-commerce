67daeee2e4ff3001fc1fff6c4bf751def48c2a68
Abhi-bot-lang <abhinav.kumar@ditstek.com>
2026-05-19 23:11:33 +0530

    Register and Login

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,16 +1,26 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from app.models.enum import  UserRole
 
 class User_Schema(BaseModel):
-    Id: int
-    FirstName: str
-    LastName: str
-    Email: str
-    Password: str
-    Role: UserRole
+    firstName: str
+    lastName: str
+    email: str
+    password: str
+    role: UserRole
 
 class LoginSchema(BaseModel):
-    Email:str
-    Password:str
+   email: str
+   password: str
 
+class VerifyOTPSchema(BaseModel):
+    email: EmailStr
+    otp:str
+
+class ForgotPasswordSchema(BaseModel):
+    email: EmailStr
+
+class ResetPasswordSchema(BaseModel):
+    email: EmailStr
+    otp: str
+    new_password: str
 
